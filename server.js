d8e0521f626ef1ab18f759c58c0b35e39f83aaf3
Matthew Cole <matthewcole@Matthews-MacBook-Pro.local>
2022-12-05 19:23:07 -0800

    fixing connection bugs

--- server.js
+++ server.js
@@ -14,14 +14,19 @@ const PORT = process.env.PORT || 3306;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: 'Tehe a little secret',
-    cookie: {},
+    secret: 'Super secret secret',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-        db: sequelize
+      db: sequelize
     })
-};
+  };
 
 app.use(session(sess));
 
