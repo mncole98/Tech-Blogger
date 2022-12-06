72b7f9ab6bdc3537a50a0b89431d5ab0c8094a61
Matthew Cole <matthewcole@Matthews-MacBook-Pro.local>
2022-12-05 20:11:21 -0800

    connection

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,9 @@ if(process.env.JAWSDB_URL) {
         process.env.DB_NAME,
         process.env.DB_USER,
         process.env.DB_PW,
-       
+       {
+        host: 'localhost',
+        dialect: 'mysql'}
     );
 }
 
