95a051632f4319757a9a054ac10bd36a5c6c02e0
Matthew Cole <matthewcole@Matthews-MacBook-Pro.local>
2022-12-06 19:44:15 -0800

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,18 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
-
-let sequelize;
-
-if(process.env.JAWSDB_URL) {
-    sequelize = new sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PW,
-       {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    });
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connection;
+//make connection
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "tech_blogger"
+  })
 }
-
-module.exports = sequelize;
+connection.connect();
+// Export connection for our ORM to use.
+module.exports = connection;
